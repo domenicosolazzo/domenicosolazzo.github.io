2d1b83269a1950798dea3a35e0d6ccb43a4ab5ac
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-08-30 19:03:18 +0200

    Modifying the About page

--- pages/Index.jsx
+++ pages/Index.jsx
@@ -6,11 +6,26 @@ var Index = React.createClass({
       <main>
         <section id="index-title" className="clearfix border-none">
           <div className="col col-12">
-            <h1 className="animated rubberBand center">Domenico Solazzo</h1>
+            <h1 className="animated rubberBand center">
+              Hi, I am Domenico Solazzo!
+            </h1>
           </div>
         </section>
         <section id="index-code" className="clearfix border-top">
-          <h3 className="animated bounceInLeft center">Code</h3>
+          <div className="clearfix border-none">
+            <div className="col col-12">
+              <h3 className="animated bounceInLeft center">Code</h3>
+            </div>
+          </div>
+          <div className="clearfix border-none">
+            <div className="col col-12">
+              <p>
+                I love coding and feel free to check my <a href="https://github.com/domenicosolazzo" target="_blank">Github</a> account.
+                <br /><br />
+                I am always learning but nowadays, I often use Python, Javascript and Swift.
+              </p>
+            </div>
+          </div>
         </section>
         <section id="index-design" className="clearfix border-top">
           <div className="clearfix border-none">
@@ -29,20 +44,20 @@ var Index = React.createClass({
           </div>
           <div className="clearfix border-none">
 
-            <div className="col col-6">
-              <img className="circle" src="https://raw.githubusercontent.com/domenicosolazzo/practice-sketch/master/icons/gauge/Exports/Gauge.png" />
+            <div className="col col-6 center">
+              <img className="circle" src="https://raw.githubusercontent.com/domenicosolazzo/practice-sketch/master/icons/gauge/Exports/Gauge%400%2C5x.png" />
             </div>
-            <div className="col col-6">
-              <img className="circle" src="https://raw.githubusercontent.com/domenicosolazzo/practice-sketch/master/icons/switch/Exports/WithoutBackground/Colorful%20Switch.png" />
+            <div className="col col-6 center">
+              <img className="circle" src="https://raw.githubusercontent.com/domenicosolazzo/practice-sketch/master/icons/switch/Exports/WithoutBackground/Colorful%20Switch%400%2C5x.png" />
             </div>
 
           </div>
           <div className="clearfix border-none">
-            <div className="col col-6">
-              <img className="circle" src="https://raw.githubusercontent.com/domenicosolazzo/practice-sketch/master/icons/calendar/Exports/WithoutBackground/Calendar.png" />
+            <div className="col col-6 center">
+              <img className="circle" src="https://raw.githubusercontent.com/domenicosolazzo/practice-sketch/master/icons/calendar/Exports/WithoutBackground/Calendar%400%2C5x.png" />
             </div>
-            <div className="col col-6">
-              <img className="circle" src="https://raw.githubusercontent.com/domenicosolazzo/practice-sketch/master/icons/todo/Exports/iTunesArtwork-512.png" />
+            <div className="col col-6 center">
+              <img className="circle" src="https://raw.githubusercontent.com/domenicosolazzo/practice-sketch/master/icons/todo/Exports/iTunesArtwork-512%400%2C5x.png" />
             </div>
           </div>
         </section>
