dca8b7fb2663c13e9f57c3e3fa5b6da5f3037ac2
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-08-30 11:36:53 +0200

    Fixing issue with the Header

--- components/Root.jsx
+++ components/Root.jsx
@@ -1,7 +1,7 @@
 var React = require('react');
 var Router = require('react-router');
 var RouteHandler = Router.RouteHandler;
-var Header = require('./Header');
+var Header = require('./Header.jsx');
 var css = require('../css/base.css')
 
 var Root = React.createClass({
