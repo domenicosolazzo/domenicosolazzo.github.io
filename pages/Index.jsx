47458cebedb1cb19e6825bde028e48d94148b1d4
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-08-30 18:11:30 +0200

    Adding the design section

--- pages/Index.jsx
+++ pages/Index.jsx
@@ -4,8 +4,47 @@ var Index = React.createClass({
   render: function(){
     return (
       <main>
-        <section id="index-title">
-          <h1 className="animated rubberBand center">Domenico Solazzo</h1>
+        <section id="index-title" className="clearfix border-none">
+          <div className="col col-12">
+            <h1 className="animated rubberBand center">Domenico Solazzo</h1>
+          </div>
+        </section>
+        <section id="index-code" className="clearfix border-top">
+          <h3 className="animated bounceInLeft center">Code</h3>
+        </section>
+        <section id="index-design" className="clearfix border-top">
+          <div className="clearfix border-none">
+            <div className="col col-12">
+              <h3 className="animated bounceInRight center">Design</h3>
+            </div>
+          </div>
+          <div className="clearfix border-none">
+            <div className="col col-12">
+              <p>
+                In my spare time, I like to learn about design.
+                I am really far for being proficient at it but below you can find some
+                of my current works. If you like what you see and you have a Dribbble invite, please invite me!
+              </p>
+            </div>
+          </div>
+          <div className="clearfix border-none">
+
+            <div className="col col-6">
+              <img className="circle" src="https://raw.githubusercontent.com/domenicosolazzo/practice-sketch/master/icons/gauge/Exports/Gauge.png" />
+            </div>
+            <div className="col col-6">
+              <img className="circle" src="https://raw.githubusercontent.com/domenicosolazzo/practice-sketch/master/icons/switch/Exports/WithoutBackground/Colorful%20Switch.png" />
+            </div>
+
+          </div>
+          <div className="clearfix border-none">
+            <div className="col col-6">
+              <img className="circle" src="https://raw.githubusercontent.com/domenicosolazzo/practice-sketch/master/icons/calendar/Exports/WithoutBackground/Calendar.png" />
+            </div>
+            <div className="col col-6">
+              <img className="circle" src="https://raw.githubusercontent.com/domenicosolazzo/practice-sketch/master/icons/todo/Exports/iTunesArtwork-512.png" />
+            </div>
+          </div>
         </section>
       </main>
     );
