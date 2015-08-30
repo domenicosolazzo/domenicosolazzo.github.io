ddd00910e66d9756db31c00e517ece1d823f6d48
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-08-30 11:21:03 +0200

    Adding client-side js

--- components/Root.jsx
+++ components/Root.jsx
@@ -4,6 +4,10 @@ var RouteHandler = Router.RouteHandler;
 
 var Root = React.createClass({
   render: function(){
+    var initialProps = {
+      __html: safeStringify(this.props)
+    };
+
     return (
       <html>
         <head>
@@ -11,11 +15,21 @@ var Root = React.createClass({
         </head>
         <body>
           <RouteHandler {...this.props} />
+          <script
+            id='initial-props'
+            type='application/json'
+            dangerouslySetInnerHTML={initialProps} />
+          <script src='bundle.js' />
         </body>
       </html>
     );
   }
 
 });
+
+function safeStringify(obj) {
+  return JSON.stringify(obj).replace(/<\/script/g, '<\\/script').replace(/<!--/g, '<\\!--')
+}
+
 
 module.exports = Root;
