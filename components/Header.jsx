05d9a6342b1f1ff29b3693fd3499d303350cba72
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-09-05 10:37:57 +0200

    Adding Navigation component

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,22 +1,13 @@
 var React = require('react');
 var Router = require('react-router');
 var Link = Router.Link;
+var Navigation = require('./Navigation.jsx');
 
 var Header = React.createClass({
   render: function () {
     return (
         <div className="clearfix">
-          <div className="container">
-            <nav className="bg">
-              <div className="sm-col">
-                <Link to="/" className="btn py2">Home Sweet Home</Link>
-                <Link to="/about" className="btn py2">About</Link>
-              </div>
-              <div className="sm-col-right">
-                <Link to="/contact" className="btn py2">Contact Me</Link>
-              </div>
-            </nav>
-          </div>
+          <Navigation />
           <div className="clearfix">
             <header className="md-col-12 sm-col py2 px1 bg-gray bg-cover bg-center backgroundImage center white">
               <h1 className="h1 h0-responsive caps mt4 mb0 regular">Hi! I am Domenico!</h1>
